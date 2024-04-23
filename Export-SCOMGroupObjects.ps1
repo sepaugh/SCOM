fcd8faf76988ac5a331951e178337ea1ff94e195
Lorne Sepaugh <9103519+sepaugh@users.noreply.github.com>
2024-04-23 14:22:23 -0400

    Update Export-SCOMGroupObjects.ps1

--- Export-SCOMGroupObjects.ps1
+++ Export-SCOMGroupObjects.ps1
@@ -1,5 +1,7 @@
 ## Use the below to help export a list of objects in each group in SCOM. This will output individual files for each group in the outputPath of your choice. Run the below in PowerShell or the Operations Manager Shell
 $groupList = Get-SCOMGroup -DisplayName "*Windows*" # Write in here what name(s) of groups that you want to, or remove "-DisplayName *Windows*" to get all groups
+
+$date = $(Get-Date -Format yyyyMMdd)
 $tempDir = "$($env:TEMP)\MSSupport-SCOMGroupExport-$($date)"
 If (Test-Path ($tempDir)) { Remove-Item  $tempDir -Force -Recurse | Out-Null; }
 New-Item -ItemType Directory -Path $tempDir -ErrorAction SilentlyContinue | Out-Null;

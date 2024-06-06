a0ef9a71423c9aa77326497a02461805f242a2d1
Lorne Sepaugh <9103519+sepaugh@users.noreply.github.com>
2024-06-06 19:05:09 -0400

    Update Get-SCOMSupportLogs.ps1

--- Get-SCOMSupportLogs.ps1
+++ Get-SCOMSupportLogs.ps1
@@ -10,39 +10,48 @@ $date = $(Get-Date -Format yyyyMMdd)
 $tempDir = "$($env:TEMP)\SCOMSupport-$($date)"
 If (Test-Path ($tempDir)) { Remove-Item  $tempDir -Force -Recurse | Out-Null; }
 New-Item -ItemType Directory -Path $tempDir -ErrorAction SilentlyContinue | Out-Null;
+New-Item -ItemType Directory -Path "$($tempDir)\Config" -ErrorAction SilentlyContinue | Out-Null;
 
 ## Get System Information
 Write-Host "`nGetting System Info"
 Get-ComputerInfo | ConvertTo-JSON | Out-File -FilePath "$($TempDir)\ComputerInfo.json";
 
-Write-Host "`nGetting TCP,IP,Schannel,CipherSuite settings"
+Write-Host "Getting .NET and Networking settings"
 
 ## Get Schannel Registry Settings
+Write-Host "    SCHANNEL"
 Get-ChildItem -Recurse "REGISTRY::HKLM\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL" | Out-File -FilePath "$($TempDir)\Config\SCHANNEL.txt" | Out-Null;
 
 ## Get SSL Config
+Write-Host "    SSL"
 Get-ChildItem -Recurse "REGISTRY::HKLM\SYSTEM\CurrentControlSet\Control\Cryptography\Configuration\Local\SSL" | Out-File -FilePath "$($TempDir)\Config\SSL.txt" | Out-Null;
 Get-ChildItem -Recurse "REGISTRY::HKLM\SOFTWARE\Policies\Microsoft\Cryptography\Configuration\SSL" | Out-File -FilePath "$($TempDir)\Config\SSL.txt" -Append | Out-Null;
 
 ## Get FIPS Config
+Write-Host "    FIPS"
 Get-ChildItem -Recurse "REGISTRY::HKLM\SYSTEM\CurrentControlSet\Control\LSA\FIPSAlgorithmPolicy" | Out-File -FilePath "$($TempDir)\Config\FIPS.txt" | Out-Null;
 
 ## Output DNS Cache
+Write-Host "    DNS Cache"
 Get-DnsClientCache | Select * | Out-File -FilePath "$($TempDir)\Config\DNSCache.txt" | Out-Null;
 
 ## Get .NET Config
+Write-Host "    .NET Framework"
 Get-ChildItem -Recurse "REGISTRY::HKLM\SOFTWARE\WOW6432Node\Microsoft\.NETFramework\v2.0.50727" | Out-File -FilePath "$($TempDir)\Config\NETFramework_v2.0.50727.txt" | Out-Null;
 Get-ChildItem -Recurse "REGISTRY::HKLM\SOFTWARE\WOW6432Node\Microsoft\.NETFramework\v4.0.30319" | Out-File -FilePath "$($TempDir)\Config\NETFramework_v4.0.30319.txt" | Out-Null;
 Get-ChildItem -Recurse "REGISTRY::HKLM\SOFTWARE\Microsoft\.NETFramework\v2.0.50727" | Out-File -FilePath "$($TempDir)\Config\NETFramework_v2.0.50727.txt" -Append | Out-Null;
 Get-ChildItem -Recurse "REGISTRY::HKLM\SOFTWARE\Microsoft\.NETFramework\v4.0.30319" | Out-File -FilePath "$($TempDir)\Config\NETFramework_v4.0.30319.txt" -Append | Out-Null;
 
 ## Get Cipher Suites
+Write-Host "    Cipher Suites"
 Get-TlsCipherSuite | ConvertTo-JSON | Out-File "$($TempDir)\Config\CipherSuites.json" | Out-Null;
 
 ## Get TCP Settings
+Write-Host "    TCP"
 Get-NetTCPSetting | Out-File -FilePath "$($TempDir)\Config\TCPSetting.txt" | Out-Null;
 
 ## Get NetIP Config
+Write-Host "    Net IP"
 Get-NetIPConfiguration -Detailed | Out-File -FilePath "$($TempDir)\Config\NetIPConfig.txt" | Out-Null;
 
 # Grab the event log files and export
